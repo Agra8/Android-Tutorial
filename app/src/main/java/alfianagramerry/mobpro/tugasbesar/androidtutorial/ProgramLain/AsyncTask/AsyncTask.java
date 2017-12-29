9fe2fdd9eb3de19e0574bdccd42b9659c30b113e
Agra8 <a_gra@outlook.com>
2017-12-29 08:17:03 +0700

    testing 4

---
+++ app/src/main/java/alfianagramerry/mobpro/tugasbesar/androidtutorial/ProgramLain/AsyncTask/AsyncTask.java
@@ -0,0 +1,10 @@
+package alfianagramerry.mobpro.tugasbesar.androidtutorial.ProgramLain.AsyncTask;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Agra on 29/12/2017.
+ */
+
+public class AsyncTask extends AppCompatActivity{
+}
---
+++ app/src/main/java/alfianagramerry/mobpro/tugasbesar/androidtutorial/ProgramLain/Maps_Android/Maps_Android.java
@@ -0,0 +1,10 @@
+package alfianagramerry.mobpro.tugasbesar.androidtutorial.ProgramLain.Maps_Android;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Agra on 29/12/2017.
+ */
+
+public class Maps_Android extends AppCompatActivity {
+}
